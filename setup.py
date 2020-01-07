31684aaf0445d08e2c4fbaa5f97fd900667aa6d3
Aymeric Galan <aymeric.galan@gmail.com>
2020-01-07 15:59:54 +0100

    Fix setup.py requirements

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 desc = open("README.rst").read()
-requires = ['numpy>=1.13', 'scipy>=0.14.0', "configparser"]
-tests_require=['pytest>=2.3', "mock"]
+requires = ['configparser']
+tests_require=['pytest>=2.3']
 
 PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 
