532771794ae73945441c33a4b6ebd359937e961a
Aymeric Galan <aymeric.galan@gmail.com>
2020-02-18 14:49:15 +0100

    Fix missing semicolons in test install

--- test_pysap_install.sh
+++ test_pysap_install.sh
@@ -2,23 +2,23 @@
 
 
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then
-    # pip install nibabel
-    # pip install pyqtgraph
-    # pip install progressbar2
-    # pip install modopt
-    # pip install pybind11
-    pip install python-pysap
+    # pip install nibabel;
+    # pip install pyqtgraph;
+    # pip install progressbar2;
+    # pip install modopt;
+    # pip install pybind11;
+    pip install python-pysap;
 fi
 
 if [ "$TRAVIS_PYTHON_VERSION" == "3.6" ]; then
-    mkdir -p $HOME/.local/share/pysap
-    git clone https://github.com/CEA-COSMIC/pysap-data.git $HOME/.local/share/pysap/pysap-data
-    ln -s $HOME/.local/share/pysap/pysap-data/pysap-data/* $HOME/.local/share/pysap
-    rm $HOME/.local/share/pysap/__init__.py
-    ls -l $HOME/.local/share/pysap
-    pip install -b $TRAVIS_BUILD_DIR/build -t $TRAVIS_BUILD_DIR/install --no-clean --no-deps .
-    ls $TRAVIS_BUILD_DIR/install
-    ldd $TRAVIS_BUILD_DIR/install/pysparse.so
-    export PYTHONPATH="$TRAVIS_BUILD_DIR/install:$PYTHONPATH"
-    export PATH="$PATH:$TRAVIS_BUILD_DIR/build/temp.linux-x86_64-3.6/extern/bin"
+    mkdir -p $HOME/.local/share/pysap;
+    git clone https://github.com/CEA-COSMIC/pysap-data.git $HOME/.local/share/pysap/pysap-data;
+    ln -s $HOME/.local/share/pysap/pysap-data/pysap-data/* $HOME/.local/share/pysap;
+    rm $HOME/.local/share/pysap/__init__.py;
+    ls -l $HOME/.local/share/pysap;
+    pip install -b $TRAVIS_BUILD_DIR/build -t $TRAVIS_BUILD_DIR/install --no-clean --no-deps .;
+    ls $TRAVIS_BUILD_DIR/install;
+    ldd $TRAVIS_BUILD_DIR/install/pysparse.so;
+    export PYTHONPATH="$TRAVIS_BUILD_DIR/install:$PYTHONPATH";
+    export PATH="$PATH:$TRAVIS_BUILD_DIR/build/temp.linux-x86_64-3.6/extern/bin";
 fi
