f7ec0cd3dafd893d5d1cbc4c2ceeae6493884171
Aymeric Galan <aymeric.galan@gmail.com>
2020-08-27 15:10:27 +0200

    Improve test

--- test/test_Util/test_plot_util.py
+++ test/test_Util/test_plot_util.py
@@ -10,12 +10,39 @@ import matplotlib.pyplot as plt
 from slitronomy.Util import plot_util
 
 
-def test_nice_colorbar():
+def test_nice_colorbars():
     fig, ax = plt.subplots(1, 1)
     im = ax.imshow(np.random.rand(10, 10))
     plot_util.nice_colorbar(im)
     # plt.show()
     plt.close()
+    fig, ax = plt.subplots(1, 1)
+    im = ax.imshow(np.random.rand(10, 10))
+    res_map = 10*np.random.rand(10, 10)
+    plot_util.nice_colorbar_residuals(im, res_map, vmin=-6, vmax=6)
+    # plt.show()
+    plt.close()
+
+def test_std_colorbars():
+    fig, ax = plt.subplots(1, 1)
+    im = ax.imshow(np.random.rand(10, 10))
+    plot_util.std_colorbar(im)
+    # plt.show()
+    plt.close()
+    fig, ax = plt.subplots(1, 1)
+    im = ax.imshow(np.random.rand(10, 10))
+    res_map = 10*np.random.rand(10, 10)
+    plot_util.std_colorbar_residuals(im, res_map, vmin=-6, vmax=6)
+    # plt.show()
+    plt.close()
+
+def test_log_cmap():
+    cmap = plt.get_cmap('cubehelix')
+    cmap_mod = plot_util.log_cmap('cubehelix', vmin=1e-2, vmax=1e1)
+    assert cmap.name == cmap_mod.name
+    assert cmap.N == cmap_mod.N
+    assert cmap.is_gray() == cmap_mod.is_gray()
+
 
 if __name__ == '__main__':
     pytest.main()
